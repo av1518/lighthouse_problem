Moved log-likelihood and posterior probability functions, cleaned up figures and saved them. Fix chain thinning.

src/main_vii.py
@@ -3,97 +3,51 @@ import numpy as np
 import zeus
 import matplotlib.pyplot as plt
 import pandas as pd
-from scipy.optimize import minimize
-import time
 from corner import corner
-
 from matplotlib import rcParams
-import matplotlib.ticker as ticker
+
+
+# Set matplotlib params
+plt.rcParams["font.family"] = "serif"
+plt.rcParams["font.serif"] = ["Times New Roman"]  # or 'DejaVu Serif'
+plt.rcParams["text.usetex"] = False  # Keep as False if LaTeX is not installed
+plt.rcParams["mathtext.fontset"] = "stix"  # LaTeX-like fonts for math expressions
+plt.rcParams["axes.titlesize"] = 16
+plt.rcParams["axes.labelsize"] = 14
+plt.rcParams["xtick.labelsize"] = 12
+plt.rcParams["ytick.labelsize"] = 12
+plt.rcParams["legend.fontsize"] = 12
+plt.rcParams["figure.figsize"] = (8, 4)
+plt.rcParams["figure.dpi"] = 100
+plt.rcParams["lines.linewidth"] = 2
+plt.rcParams["grid.linestyle"] = "--"
+plt.rcParams["grid.alpha"] = 0.7
 
 
 from funcs import (
-    plot_posterior_contours,
-    log_likelihood,
-    log_posterior,
+    log_posterior_combined,
+    plot_posterior_vs_intensity,
     gelman_rubin_for_multiple_chains,
 )
 
-
 # Update Matplotlib settings to use LaTeX-like font
-# Set Matplotlib to use a commonly available serif font
 rcParams["font.family"] = "serif"
 rcParams["text.usetex"] = False  # Keep as False if LaTeX is not installed
-# %%
 # load the data
 file_path = "../lighthouse_flash_data.txt"
 data = pd.read_csv(file_path, sep=" ", header=None)
 flash_locations, intensities = data[0].values, data[1].values
+# %% Plot the posterior distribution vs. intensity for the best alpha and beta from the previous part
+from matplotlib.ticker import FuncFormatter
 
 
-def log_likelihood_loc(theta, data):
-    """
-    Calculate the log-likelihood for part (v).
-
-    Args:
-    @param theta A tuple or list containing the parameters alpha and beta.
-                 - alpha (float): The position of the lighthouse along the coast.
-                 - beta (float): The distance of the lighthouse from the shore.
-    @param data A numpy array of observed flash locations along the coastline.
-
-    @return The log-likelihood of the observed data given the parameters.
-    """
-    alpha, beta = theta
-    return np.log(beta / np.pi) * len(data) - np.sum(
-        np.log(beta**2 + (data - alpha) ** 2)
-    )
-
-
-def log_likelihood_int(alpha, beta, I0, x_data, I_data, sigma=1.0):
-    # calculate squared distatnce from the lighthouse to each detected flash
-    d_squared = beta * beta + (x_data - alpha) ** 2
-
-    # expected log-intensity foe each measurement
-    mu = np.log(I0) - np.log(d_squared)
-
-    log_likelihood = -1 / (2 * sigma**2) * np.sum((np.log(I_data) - mu) ** 2) - len(
-        I_data
-    ) * np.log(np.sqrt(2 * np.pi) * sigma)
-    return log_likelihood
-
-
-def combined_log_likelihood(theta, x_data, I_data):
-    alpha, beta, I0 = theta
-    loc_likelihood = log_likelihood_loc((alpha, beta), x_data)
-    int_likelihood = log_likelihood_int(alpha, beta, I0, x_data, I_data)
-    return loc_likelihood + int_likelihood
-
-
-def log_posterior_combined(
-    theta,
-    loc_data,
-    int_data,
-    alpha_min=-10,
-    alpha_max=10,
-    beta_min=0.01,
-    beta_max=10,
-    I0_min=0.01,
-    I0_max=100,
-):
-    alpha, beta, I0 = theta
-
-    if (
-        beta_min < beta < beta_max
-        and alpha_min < alpha < alpha_max
-        and I0_min < I0 < I0_max
-    ):
-        combined_likelihood = combined_log_likelihood(theta, loc_data, int_data)
-        # log_prior_I0 = -np.log(sigma_I0 * np.sqrt(2 * np.pi)) - (
-        #     np.log(I0) - mu_I0
-        # ) ** 2 / (2 * sigma_I0**2)
-        jeffrey_prior = np.log(1 / (I0 * np.log(I0_max / I0_min)))
-        return combined_likelihood + jeffrey_prior
+def scientific_notation(x, pos):
+    if x != 0:
+        exponent = int(np.floor(np.log10(np.abs(x))))
+        coeff = x / 10**exponent
+        return r"${:.0f}\times10^{{{}}}$".format(coeff, exponent)
     else:
-        return -np.inf
+        return r"$0$"
 
 
 def plot_posterior_vs_intensity(
@@ -117,17 +71,28 @@ def plot_posterior_vs_intensity(
 
     # Plot the results
     plt.figure(figsize=(8, 6), dpi=150)
-    plt.plot(I0_vals, posterior, label="Posterior vs. I0")
+    plt.plot(
+        I0_vals,
+        posterior,
+        label=r" Posterior with fixed $\alpha$ and $\beta$",
+        color="blue",
+    )
     plt.xlabel(r"$I_0$", fontsize=15)
-    plt.ylabel("Posterior Probability", fontsize=15)
-    plt.title("Posterior Distribution vs. Intensity (I0)")
+    plt.ylabel(
+        r"$\mathcal{L}(\{ log(I_k) | \hat{\alpha}, \hat{\beta}, I_0)   $ ", fontsize=15
+    )
+    # plt.title("Posterior Distribution vs. Intensity (I0)")
+
+    # Set the y-axis formatter to scientific notation
+    plt.gca().yaxis.set_major_formatter(FuncFormatter(scientific_notation))
+
     plt.legend()
+    plt.savefig("posterior_vs_intensity.png", dpi=300, bbox_inches="tight")
     plt.show()
 
 
-# %%
 plot_posterior_vs_intensity(log_posterior_combined, flash_locations, intensities)
-# %%
+# %% MCMC sampling
 nwalkers = 10
 nsteps = 10000
 ndim = 3
@@ -135,7 +100,6 @@ ndim = 3
 alpha_start_bounds = [-5, 5]
 beta_start_bounds = [0.02, 5]
 I0_start_bounds = [2, 8]
-
 
 # Generating initial positions within the bounds
 start_positions = np.zeros((nwalkers, ndim))
@@ -149,44 +113,135 @@ start_positions[:, 2] = np.random.uniform(
     I0_start_bounds[0], I0_start_bounds[1], nwalkers
 )  # I0
 
-
 sampler = zeus.EnsembleSampler(
     nwalkers, ndim, log_posterior_combined, args=[flash_locations, intensities]
 )
-# sampler2 = zeus.EnsembleSampler(nwalkers, ndim, log_posterior, args=[flash_locations])
+sampler_2 = zeus.EnsembleSampler(
+    nwalkers, ndim, log_posterior_combined, args=[flash_locations, intensities]
+)
+# saplter2 = zeus.EnsembleSaplter(nwalkers, ndim, log_posterior, args=[flash_locations])
+print("Running first sampler...")
 sampler.run_mcmc(start_positions, nsteps)
-# sampler2.run_mcmc(start_positions, nsteps)
-
-# %%
+print("Running second sampler (for use in diagnostics)...")
+sampler_2.run_mcmc(start_positions, nsteps)
+# %% Autocorrelation time
 taus = zeus.AutoCorrTime(sampler.get_chain())
-print("Autocorrelation:", taus)
-
+print("AutoCorrelation time :")
+print("tau_alpha = {}, tau_beta = {}, tau_I0 = {}".format(*taus))
 tau = max(taus)
-print(f"{tau = }")
+print(f"Max {tau = }")
 # %% Gelman rubin test
-iid = sampler.get_chain(flat=True)
-num_samples = len(iid)
-print(f"{num_samples = }")
+chain_1 = sampler.get_chain(flat=True)
+chain_2 = sampler_2.get_chain(flat=True)
+print(len(chain_1), len(chain_2))
+# %% Visualize the chains
 
-# iid_2 = sampler2.get_chain(flat=True, thin=int(tau), discard=0.2)
+fig, axs = plt.subplots(nrows=3, figsize=(7, 10))
+
+for i in range(3):
+    axs[0].plot(sampler.get_chain()[:, i, 0][:500], alpha=0.5, label=f"Walker {i+1}")
+axs[0].set_xlabel("Iterations")
+axs[0].set_ylabel(r"$\alpha$")
+axs[0].legend()
+
+for i in range(3):
+    axs[1].plot(sampler.get_chain()[:, i, 1][:500], alpha=0.5, label=f"Walker {i+1}")
+axs[1].set_xlabel("Iterations")
+axs[1].set_ylabel(r"$\beta$")
+axs[1].legend()
+
+for i in range(3):
+    axs[2].plot(sampler.get_chain()[:, i, 2][:500], alpha=0.5, label=f"Walker {i+1}")
+axs[2].set_xlabel("Iterations")
+axs[2].set_ylabel(r"$I_0$")
+axs[2].legend()
+
+plt.tight_layout()
+plt.savefig("chain_plots.png", dpi=500, bbox_inches="tight")
+plt.show()
 
 # %%
-# alpha_chains = [iid[:, 0], iid_2[:, 0]]
-# beta_chains = [iid[:, 1], iid_2[:, 1]]
+alpha_chains_1 = [chain_1[:, 0], chain_2[:, 0]]
+beta_chains_1 = [chain_1[:, 1], chain_2[:, 1]]
+I0_chains_1 = [chain_1[:, 2], chain_2[:, 2]]
 
-alpha_mean, beta_mean, I0_mean = np.mean(iid, axis=0)
-alpha_std, beta_std, I0_std = np.std(iid, axis=0)
+GR_alpha = gelman_rubin_for_multiple_chains(alpha_chains_1, frequency=100)
+GR_beta = gelman_rubin_for_multiple_chains(beta_chains_1, frequency=100)
+GR_I0 = gelman_rubin_for_multiple_chains(I0_chains_1, frequency=100)
+
+
+def plot_gelman_rubin(gr_stats_alpha, gr_stats_beta, gr_stats_I0, max_iteration):
+    fig, axes = plt.subplots(3, 1, figsize=(8, 12), dpi=500)
+
+    # Alpha subplot
+    iterations, R_values = zip(*gr_stats_alpha)
+    axes[0].plot(
+        iterations[:max_iteration], R_values[:max_iteration], marker="o", color="black"
+    )
+    axes[0].set_xlabel("Iteration")
+    axes[0].set_ylabel(r"$\hat{R}(\alpha)$ ")
+    axes[0].grid(True)
+
+    # Beta subplot
+    iterations, R_values = zip(*gr_stats_beta)
+    axes[1].plot(
+        iterations[:max_iteration], R_values[:max_iteration], marker="o", color="black"
+    )
+    axes[1].set_xlabel("Iteration")
+    axes[1].set_ylabel(r"$\hat{R}(\beta)$ ")
+    axes[1].grid(True)
+
+    # I0 subplot
+    iterations, R_values = zip(*gr_stats_I0)
+    axes[2].plot(
+        iterations[:max_iteration], R_values[:max_iteration], marker="o", color="black"
+    )
+    axes[2].set_xlabel("Iteration")
+    axes[2].set_ylabel(r"$\hat{R}(I_0)$")
+    axes[2].grid(True)
+
+    # Set a single, overarching title
+    fig.suptitle("Gelman-Rubin Diagnostics", fontsize=16)
+
+    fig.subplots_adjust(hspace=0.4)
+
+    plt.tight_layout(rect=[0, 0.03, 1, 0.95])
+
+    plt.savefig("Gelman-Rubin_Diagnostics.png", bbox_inches="tight")
+    plt.show()
+
+
+# Example usage
+plot_gelman_rubin(GR_alpha, GR_beta, GR_I0, 100)  # Plots up to the 100th iteration
+
+# %%
+chain_iid = sampler.get_chain(
+    flat=True, thin=int(tau), discard=0.02
+)  # discard 2% of the chain (burn-in)
+print("Number of iid samples after discard burnin and thinning =", len(chain_iid))
+
+
+alpha_mean, beta_mean, I0_mean = np.mean(chain_iid, axis=0)
+alpha_std, beta_std, I0_std = np.std(chain_iid, axis=0)
 print(f"Alpha: Mean = {alpha_mean}, Std = {alpha_std}")
 print(f"Beta: Mean = {beta_mean}, Std = {beta_std}")
 print(f"I0: Mean = {I0_mean}, Std = {I0_std}")
 
+# save the means and stds to a file
+with open("means_stds_vii.txt", "w") as file:
+    file.write(
+        f"Alpha: Mean = {alpha_mean}, Std = {alpha_std}\n"
+        f"Beta: Mean = {beta_mean}, Std = {beta_std}\n"
+        f"I0: Mean = {I0_mean}, Std = {I0_std}"
+    )
+
 corner(
-    iid,
+    chain_iid,
     labels=[r"$\alpha$", r"$\beta$", r"$I_0$"],
     truths=[alpha_mean, beta_mean, I0_mean],
+    show_titles=True,
+    bins=40,
+    color="black",
 )
+plt.savefig("corner_plot_vii.png", dpi=500, bbox_inches="tight")
 plt.show()
-# %%
-
-GR_alpha = gelman_rubin_for_multiple_chains(alpha_chains, frequency=10)
-GR_beta = gelman_rubin_for_multiple_chains(beta_chains, frequency=10)